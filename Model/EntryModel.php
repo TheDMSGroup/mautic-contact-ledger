4c4b601ea127e1453452e04d7dbd24c98d68fb2a
Nicholai Bush <nbush@thedmsgrp.com>
2018-03-03 11:41:49 -0500

    structured

--- Model/EntryModel.php
+++ Model/EntryModel.php
@@ -2,34 +2,81 @@
 
 namespace MauticPlugin\MauticContactLedgerBundle\Model;
 
-
+use Mautic\LeadBundle\Entity\Lead;
 use Mautic\CoreBundle\Model\AbstractCommonModel;
-
+use MauticPlugin\MauticContactLedgerBundle\Entity\Entry;
+/**
+ * class EntryModel extends {@see \Mautic\CoreBundle\Model\AbstractCommonModel}
+ *
+ * @package \MauticPlugin\MauticContactLedgerBundle\Model
+ */
 class EntryModel extends AbstractCommonModel
 {
     /**
      * {@inheritdoc}
      *
-     * @return \Mautic\CoreBundle\Entity\AuditLogRepository
+     * @return \MauticPlugin\MauticContactLedgerBundle\Entity\EntryRepository
      */
     public function getRepository()
     {
         return $this->em->getRepository('MauticContactLedgerBundle:Entry');
     }
-
-    /**
-     * this is a revenue entry
-     */
-    public function enterDebit()
+    
+    
+    public function buildActorFromObject($object_instance)
     {
+        $class = get_class($object_instance);
+        
+    }
+    
+    public function buildActorFromIdClass(int $id, string $class)
+    {
+        if (!in_array($class, get_declared_classes())) {
+            $a = true;
+        }
+    }
 
+    protected function findBundle($class)
+    {
+            
     }
 
     /**
-     * this is a cost entry
+     * Writes a revenue (credit) to the ledger
+     *
+     * @param \Mautic\LeadBundle\Entity\Lead    $lead       target of transaction
+     * @param string|float                      $amount     decimal dollar amount of tranaction
+     * @param string                            $activity   cause for transaction
+     * @param mixed                             $actor      [bundle, object, id] that acted
      */
-    public function enterCredit()
+    public function writeRevenue(Lead $lead, $amount, $activity, $actor)
+    {
+        if (is_object($actor)) {
+            
+        }
+        elseif (is_array($actor)) {
+            
+        }
+        
+        $entry = $this->getRepository()->getEntity(0);
+        $entry->setCost($amount)
+            ->setContact($lead)
+            ->setActivity($activity);
+            
+    }
+
+    /**
+     * Writes a cost (debit) to the ledger
+     *
+     * @param \Mautic\LeadBundle\Entity\Lead    $lead       target of transaction
+     * @param string|float                      $amount     decimal dollar amount of tranaction
+     * @param string                            $activity   cause for transaction
+     * @param mixed[]                           $actor      [bundle, object, id] that acted
+     */
+    public function writeCost(Lead $lead, $amount, $activity, $actor)
     {
 
     }
+    
+    
 }
