04270be01bd4dcecc885d9dc345f63c4fafea27f
Nicholai Bush <nicholaibush@yahoo.com>
2018-03-21 10:39:50 -0400

    context support for ledger - untested

---
+++ Event/ContactLedgerContextEventInterface.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nbush
+ * Date: 3/21/18
+ * Time: 12:19 AM
+ */
+
+namespace MauticPlugin\MauticContactLedgerBundle\Event;
+
+
+class ContactLedgerContextEventInterface
+{
+
+}
