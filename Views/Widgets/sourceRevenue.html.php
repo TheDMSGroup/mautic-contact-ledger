ff09f5039b6820648a100bc99a628d5f49ea2df8
scottshipman <k.scott.shipman@gmail.com>
2018-04-23 11:08:44 -0400

    tune down the spinner

--- Views/Widgets/sourceRevenue.html.php
+++ Views/Widgets/sourceRevenue.html.php
@@ -20,7 +20,7 @@ $params = $data['params'];
         <div id="campaign-source-revenue-table" style="height:<?php echo $data['height']; ?>px">
             <!-- Revenue By Campaign + Source -->
             <div class="responsive-table">
-                <div id="source-builder-overlay"><div style="position: relative; top: <?php echo $data['height'] / 3; ?>px; left: 45%; index: 1024;display:inline-block;"><i class="fa fa-spinner fa-spin fa-5x"></i></div></div>
+                <div id="source-builder-overlay"><div style="position: relative; top: <?php echo $data['height'] / 3; ?>px; left: 45%; index: 1024;display:inline-block; opacity: .5;"><i class="fa fa-spinner fa-spin fa-4x"></i></div></div>
                 <table id="source-revenue" class="table table-striped table-bordered" width="100%">
                 </table>
             </div>
